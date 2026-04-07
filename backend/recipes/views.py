0329bed51b085281dd9d25dd4e46905857236af6
Waxxer3 <waxxer3@gmail.com>
2026-04-07 21:21:33 +0300

    fix

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -31,6 +31,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     permission_classes = (permissions.AllowAny,)
+    pagination_class = None
     filter_backends = (SearchFilter,)
     search_fields = ('^name',)
 

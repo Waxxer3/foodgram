29df910eb0091f66c3bb9bee506019f5fbbce87c
Waxxer3 <waxxer3@gmail.com>
2026-04-09 12:59:22 +0300

    Fix urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -32,11 +32,9 @@ router.register('tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include([
-        path('auth/', include('djoser.urls.authtoken')),
-        path('', include('djoser.urls')),
-        path('', include(router.urls)),
-    ])),
+    path('api/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/', include(router.urls)),
 ]
 
 if settings.DEBUG:
